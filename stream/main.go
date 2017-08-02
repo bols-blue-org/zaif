88187c2b51ad0147a27bff0cca223ced4886fb17
hisayoshi suehiro <hisayoshi.suehiro@access-company.com>
2017-08-02 18:25:15 +0900

    first commit

---
+++ stream/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"golang.org/x/net/websocket"
+)
+
+func readBoad(ws *websocket.Conn) []byte {
+	var msg = make([]byte, 1024*4)
+	var n int
+	var err error
+	if n, err = ws.Read(msg); err != nil {
+		log.Fatal(err)
+	}
+	var readData = make([]byte, n)
+	copy(readData, msg)
+	var data []byte
+	if n == 4092 {
+		if n, err = ws.Read(msg); err != nil {
+			log.Fatal(err)
+		}
+		data = append(readData, msg[:n]...)
+	}
+	if n == 4092 {
+		if n, err = ws.Read(msg); err != nil {
+			log.Fatal(err)
+		}
+		data = append(readData, msg[:n]...)
+	}
+	return data
+}
+
+func printBoad(data []byte) {
+	var query map[string]interface{}
+	json.Unmarshal(data, &query)
+	if query["bids"] != nil {
+		var bids []interface{}
+		bids = query["bids"].([]interface{})
+		fmt.Printf("%v\n", bids[:3])
+
+	}
+	if query["asks"] != nil {
+		var asks []interface{}
+		asks = query["asks"].([]interface{})
+		fmt.Printf("%v\n", asks[:3])
+	}
+}
+
+func main() {
+	origin := "http://ws.zaif.jp/"
+	url := "wss://ws.zaif.jp:8888/stream?currency_pair=xem_btc"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*
+		var message string
+		websocket.Message.Receive(ws, &message)
+		fmt.Printf("%s\n", message)
+	*/
+	printBoad(readBoad(ws))
+	printBoad(readBoad(ws))
+	printBoad(readBoad(ws))
+	printBoad(readBoad(ws))
+	printBoad(readBoad(ws))
+	printBoad(readBoad(ws))
+}
